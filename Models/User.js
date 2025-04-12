4f5b32f9c5bc5986e3584a544bf2f9b7f9327c2c
Shreya ojha <shreyaojha192027@gmail.com>
2025-04-12 21:45:29 +0530

    first commit

---
+++ Models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 4
+    },
+    age: {
+        type: Number,
+        required: true
+    }
+}, { timestamps: true });
+
+// Pre-save hook to hash password
+// UserSchema.pre('save', function (next) {
+//     if (!this.isModified('password')) {
+//         return next();
+//     }
+//     const salt = bcrypt.genSaltSync(10);
+//     this.password = bcrypt.hashSync(this.password, salt);
+//     next();
+// });
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
